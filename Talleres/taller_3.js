2a6e48377b4101a8a9edbcb2516ab88ff56e8756
YordiG <yjyordi1703@gmail.com>
2025-08-29 18:47:33 -0500

    Taller 3

---
+++ Talleres/taller_3.js
@@ -0,0 +1,42 @@
+/*
+Punto 1:
+Desarrolle una funcion llamada desglosarString que reciba una string, y la string "vocales" o "consonantes", y
+retorne la cantidad de vocales o consonantes de la string recibida. 
+*/
+function desglosarString(palabra, filtro){
+    if(filtro === "vocales"){
+        return 
+    }else if(filtro === "consonantes"){
+
+    }
+}
+
+/*
+Punto 2:
+Desarrolle una funcion llamada twoSum que reciba una lista de numero enteros y otro numero entero y retorne
+los indices de los numeros del arreglo que sumados sean el otro numero.
+*/
+
+
+
+
+
+
+/*
+punto 3:
+esarrolle una funcion llamada conversionRomana que reciba una string de cifras romanas y retorne el
+equivalente en cifras arábigas.
+*/
+
+
+
+
+
+
+
+/*
+punto 4:
+Desarrolle una funcion llamada descomposicion que reciba una string de palabras separada por comas, donde
+la primera palabra es la palabra a descomponer y el resto son el diccionario a utilizar. La función debe retornar
+las dos palabras del diccionario que compongan la palabra a descomponer.
+*/
